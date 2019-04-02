c00a47366e46b90951fc2f3f4eb86451d09d0b00
John Finlay <finlay_john@hotmail.com>
2019-04-02 07:30:23 -0500

    Add all routes

--- application.py
+++ application.py
@@ -17,7 +17,54 @@ session = DBSession()
 def homePage():
     return 'Home Page'
 
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    return 'Login Page'
 
+@app.route('/logout', methods = ['POST'])
+def logout():
+    return 'Logout'
+
+@app.route('/<category>/items')
+def categoryItems(category):
+    return 'List of items for category'
+
+@app.route('/<category>/<item>')
+def itemDetail(category, item):
+    return 'Item Details Page'
+
+@app.route('/<category>/items/new', methods = ['GET','POST'])
+def newItem():
+    return 'New Item Page'
+
+@app.route('/<category>/<item>/edit', methods = ['GET','POST'])
+def editItem(category, item):
+    return 'Edit Item Page'
+
+@app.route('/<category>/<item>/delete', methods = ['GET','POST'])
+def editItem(category, item):
+    return 'Delete Item Page'
+
+@app.route('/api/categories')
+def apiCategories():
+    return 'Return all categories'
+
+@app.route('/api/items')
+def apiItems():
+    return 'Return all items'
+
+@app.route('/api/categories/<int:category_id>')
+def apiCategory(category_id):
+    return 'Return single category info.'
+
+
+@app.route('/api/categories/<int:category_id>/items')
+def apiCategoryItems(category_id):
+    return 'Return all items for a category'
+
+@app.route('/api/items/<int:item_id>')
+def apiItem(item_id):
+    return 'Return single item detail'
 
 
 if __name__ == '__main__':
