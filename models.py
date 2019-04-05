6fdf11b6cb31de3b6d2eaaa6ce165311e00a0b0d
John Finlay <finlay_john@hotmail.com>
2019-04-05 07:07:53 -0500

    Add date_created for Item, auto-populate categories.

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column,Integer,String, ForeignKey
+from sqlalchemy import Column,Integer,String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
+import datetime
 
 Base = declarative_base()
 
@@ -30,6 +31,7 @@ class Item(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250))
     description = Column(String(250))
+    date_created = Column(DateTime, default=datetime.datetime.utcnow)
     user_id = Column(Integer, ForeignKey('user.id'))
     category_id = Column(Integer, ForeignKey('category.id'))
     user = relationship(User)
@@ -48,4 +50,20 @@ engine = create_engine('sqlite:///catalog.db')
  
 
 Base.metadata.create_all(engine)
-    
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+session.add_all([
+    Category(name='Soccer'),
+    Category(name='Basketball'),
+    Category(name='Baseball'),
+    Category(name='Frisbee'),
+    Category(name='Snowboarding'),
+    Category(name='Rock Climbing'),
+    Category(name='Foosball'),
+    Category(name='Skating'),
+    Category(name='Hockey')
+])
+session.commit()
